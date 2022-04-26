e01429888613bb8bc2ad5ec938535751aeebc7dc
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-04-26 13:07:43 +0200

    remove index.scss and modified carrousel if one image

--- kasa-app/src/components/Carousel.jsx
+++ kasa-app/src/components/Carousel.jsx
@@ -8,8 +8,9 @@ function Carousel({pictures, title}) {
     return (
         <div className="carousel">
             <img src={pictures[count]} alt={title} />
-            <button className="next" onClick={() => setCount(count === pictures.length - 1 ? 0 : count + 1)}><FaChevronRight className="next-icon" /></button>
-            <button className="prev" onClick={() => setCount(count === 0 ?  pictures.length  - 1 : count - 1)}><FaChevronLeft className="prev-icon" /></button>
+            {pictures.length !==  1 && <button className="next" onClick={() => setCount(count === pictures.length - 1 ? 0 : count + 1)}><FaChevronRight className="next-icon" /></button>}
+            {pictures.length !==  1 && <button className="prev" onClick={() => setCount(count === 0 ?  pictures.length  - 1 : count - 1)}><FaChevronLeft className="prev-icon" /></button>}
+            
             <span className="index-img-carousel">{count + 1}/{pictures.length}</span>
         </div>
     )
